e60e1e747c47dbc38699645c17eae7220620d560
Fritsje <perrychris@live.nl>
2020-12-12 18:01:46 +0100

    ImageCracker added

---
+++ ImageCracker.py
@@ -0,0 +1,53 @@
+import subprocess
+import sys, getopt
+
+def main(argv):
+	imagefile = ''
+	wordlist = ''
+	opts, args = getopt.getopt(argv, "f:w:", ["file=", "wordlist="])
+	for opt, arg in opts:
+		if opt in ("-f", "--file"):
+			imagefile = arg
+		elif opt in ("-w", "--wordlist"):
+			wordlist = arg
+
+	if imagefile == '' or wordlist == '':
+		print('Usage: imagecracker.py -f <file> -w <wordlist>')
+		sys.exit()
+
+	try:
+		file = open(str(wordlist), 'r')
+	except:
+		print('Could not find file: {}'.format(wordlist))
+		sys.exit(2)
+
+	try:
+		open(str(imagefile), 'r')
+	except:
+		print('Could not find file: {}'.format(imagefile))
+		sys.exit(2)
+
+	print('Starting bruteforce attack...\n')
+
+	while True:
+		line = file.readline()
+		password = line.strip()
+
+		cmd = ['steghide', 'info', str(imagefile), '-p', password]
+		proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+		o, e = proc.communicate()
+
+		if not line:
+			print('Could not find password in the given wordlist...')
+			break
+
+		if e.decode('ascii') != 'steghide: could not extract any data with that passphrase!\n':
+			print('Output: ' + o.decode('ascii'))
+			print('Found password: {}'.format(password))
+			break
+
+	file.close()
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
